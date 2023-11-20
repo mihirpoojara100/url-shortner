c88b62f82061deaf8decd59efefd817dde008400
Mihir Poojara <mihirpoojara100@gmail.com>
2023-11-20 22:23:35 +0530

    BUG FIXES

--- src/helpers/common.helper.ts
+++ src/helpers/common.helper.ts
@@ -3,11 +3,11 @@ import * as md5 from 'md5';
 let counter = 1;
 export default class Helper {
   getSmallUrlAndHash(long_url: string) {
-    let md5Hash = md5(long_url);
+    const md5Hash = md5(long_url);
 
-    md5Hash = md5Hash + counter;
+    const md5HashUpdated = counter + md5Hash.slice(0, 8); // converts into base64
 
-    const short_url = btoa(md5Hash).slice(0, 8); // converts into base64
+    const short_url = btoa(md5HashUpdated);
 
     counter++;
 
